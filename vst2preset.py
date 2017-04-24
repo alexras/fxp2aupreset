755a8f861d0a0a38983c321f622a8a44e6d85032
Walter Cruz <waltercruz@gmail.com>
2017-04-24 10:33:46 -0300

    updating construct to 2.8

--- vst2preset.py
+++ vst2preset.py
@@ -3,40 +3,40 @@
 # based on VST SDK's vst2.x/vstfxstore.h
 # names as in the source
 
-from construct import Array, BFloat32, Bytes, Const, Container, Enum, \
-    LazyBound, String, Struct, Switch, UBInt32, ULInt32
+from construct import Array, Float32b, Bytes, Const, Container, Enum, \
+    LazyBound, String, Struct, Switch, Int32ub, Int32ul
 
-vst2preset = Struct('vst2preset',
-    Const(Bytes('chunkMagic', 4), 'CcnK'),
-    UBInt32('byteSize'),
-    Enum(Bytes('fxMagic', 4),
-        FXP_PARAMS = 'FxCk', FXP_OPAQUE_CHUNK = 'FPCh',
-        FXB_REGULAR = 'FxBk', FXB_OPAQUE_CHUNK = 'FBCh',
-        ),
-    UBInt32('version'),
-    UBInt32('fxID'),
-    UBInt32('fxVersion'),
-    UBInt32('count'),
-    Switch('data', lambda ctx: ctx['fxMagic'], {
-        'FXP_PARAMS': Struct('data',
-            String('prgName', 28, padchar = '\0'),
-            Array(lambda ctx: ctx['_']['count'], BFloat32('params')),
-            ),
-        'FXP_OPAQUE_CHUNK': Struct('data',
-            String('prgName', 28, padchar = '\0'),
-            UBInt32('size'),
-            Bytes('chunk', lambda ctx: ctx['size']),
-            ),
-        'FXB_REGULAR': Struct('data',
-            Bytes('future', 128), # zeros
-            # Array of FXP_PARAMS vst2preset
-            Array(lambda ctx: ctx['_']['count'], LazyBound('presets', lambda: vst2preset)),
-            ),
-        'FXB_OPAQUE_CHUNK': Struct('data',
-            Bytes('future', 128), # zeros
-            UBInt32('size'),
-            # Unknown format of internal chunk
-            Bytes('chunk', lambda ctx: ctx['size']),
-            ),
-        }),
+vst2preset = Struct(
+    'chunkMagic' / Const(b"CcnK"),
+    "byteSize" / Int32ub,
+    "fxMagic" / Enum(Bytes(4),
+       FXP_PARAMS = b'FxCk', FXP_OPAQUE_CHUNK = b'FPCh',
+       FXB_REGULAR = b'FxBk', FXB_OPAQUE_CHUNK = b'FBCh',
+    ),
+    "version" / Int32ub,
+    "fxID" / Int32ub,
+    "fxVersion" / Int32ub,
+    "count" / Int32ub,
+    # Switch('data', lambda ctx: ctx['fxMagic'], {
+    #     'FXP_PARAMS': Struct('data',
+    #         String('prgName', 28, padchar = '\0'),
+    #         Array(lambda ctx: ctx['_']['count'], Float32b('params')),
+    #         ),
+    #     'FXP_OPAQUE_CHUNK': Struct('data',
+    #         String('prgName', 28, padchar = '\0'),
+    #         Int32ub('size'),
+    #         Bytes('chunk', lambda ctx: ctx['size']),
+    #         ),
+    #     'FXB_REGULAR': Struct('data',
+    #         Bytes('future', 128), # zeros
+    #         # Array of FXP_PARAMS vst2preset
+    #         Array(lambda ctx: ctx['_']['count'], LazyBound('presets', lambda: vst2preset)),
+    #         ),
+    #     'FXB_OPAQUE_CHUNK': Struct('data',
+    #         Bytes('future', 128), # zeros
+    #         Int32ub('size'),
+    #         # Unknown format of internal chunk
+    #         Bytes('chunk', lambda ctx: ctx['size']),
+    #         ),
+    #     }),
     )
